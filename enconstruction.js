6f45d108632ec12ebed48af77305576f96059bde
laura-azib <laura.azib@gmail.com>
2021-11-05 11:44:19 +0100

    ajout fonction des boutons et création de la boite de dialogue

--- enconstruction.js
+++ enconstruction.js
@@ -1,3 +1,51 @@
-document.getElementById("button").onclick = function(){
-    document.getElementById("suitesite").innerHTML="Bonjour "+champdesaisie.value;
+function messageBienvenue(){
+
+    document.getElementById("bonjourprenom").innerHTML="Bonjour "+champdesaisie.value;
+
+}
+
+    document.getElementById("button").addEventListener('click', messageBienvenue);
+    
+
+
+
+
+    function cacheBloc() {
+        fourthdiv.style.display = "none";
+        fifthdiv.style.display = "none";
     }
+    cacheBloc();
+    
+    function afficheBloc(){
+        fourthdiv.style.display = "flex";
+        fifthdiv.style.display = "flex";
+    }
+    document.getElementById("button").addEventListener("click", afficheBloc);
+
+
+
+
+
+
+    function messageDialogue(){
+
+        document.getElementById("boitedialogue").innerHTML="Bonjour "+champdesaisie.value + ", nous te confirmons que tu seras informé sur ton e-mail : "+email.value;
+    
+    }
+    
+        document.getElementById("button2").addEventListener('click', messageDialogue);
+
+
+    
+
+        function cacheDialogue() {
+            boitedialogue.style.display = "none";
+           
+        }
+        cacheDialogue();
+        
+        function afficheDialogue(){
+            boitedialogue.style.display = "flex";
+            
+        }
+        document.getElementById("button2").addEventListener("click", afficheDialogue);
