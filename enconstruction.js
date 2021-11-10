d7a599a81616b22ee883f6fe5874ade06e8ed7be
laura-azib <laura.azib@gmail.com>
2021-11-10 09:46:24 +0100

    finalisation de l'exercice et enregistrement du choix client

--- enconstruction.js
+++ enconstruction.js
@@ -30,7 +30,6 @@ function messageBienvenue(){
 
 
 //La fonction ici veut dire que dans le paragraphe vide textedialogue, s'affichera bonjour + la valeur indiquée dans la barre du prénom + la valeur indiquée dans la barre de l'e-mail
-
     function messageDialogue(){
 
         document.getElementById("textedialogue").innerHTML="Bonjour "+champdesaisie.value + " ! Nous te confirmons que tu seras informé sur ton e-mail : "+email.value;
@@ -42,7 +41,6 @@ function messageBienvenue(){
 
     
 //Ici cette fonction sert à cacher les div mentionnées avec la fonction "none"
-
         function cacheDialogue() {
             elementdialogue.style.display = "none";
            
@@ -57,13 +55,11 @@ function messageBienvenue(){
             
         }
 //Quand on clique sur le deuxième bouton la fonction ci-dessus s'éxecute    
-
         document.getElementById("button2").addEventListener("click", afficheDialogue);
 
 
 
 //Ici cette fonction sert à cacher les div mentionnées avec la fonction "none"
-
         function cacheDialogue2(){
             elementdialogue.style.display = "none";
             fourthdiv.style.display = "none";
@@ -72,14 +68,10 @@ function messageBienvenue(){
             fourthdiv.style.display = "none";
             seconddiv.style.display = "none";
             div.style.display = "none";
-            thirddiv.style.display = "none";
-            
-        
+            thirddiv.style.display = "none";    
         }
 
-
 //Quand on clique sur le troisième bouton la fonction ci-dessus s'éxecute    
-
         document.getElementById("button3").addEventListener("click", cacheDialogue2);
 
 
@@ -89,7 +81,6 @@ function messageBienvenue(){
         function messageAurevoir(){
 
             document.getElementById("bonjourprenom2").innerHTML="Merci "+champdesaisie.value +" et à bientôt !";
-        
         }
         
             document.getElementById("button3").addEventListener('click', messageAurevoir);
@@ -100,18 +91,13 @@ function messageBienvenue(){
 
 
 //Ici cette fonction sert à cacher les div mentionnées avec la fonction "none" soit en l'occurence la barre de cookie
-
 function cacheDialogue3(){
     barrecookie.style.display = "none";
     
 }
 
 //Quand on clique sur le quatrième bouton la fonction ci-dessus s'éxecute    
-
 document.getElementById("button4").addEventListener("click", cacheDialogue3);
-
-
-
 
 
 
@@ -122,16 +108,21 @@ function afficheModale(){
     
 }
 //Quand on clique sur le bouton la fonction ci-dessus s'éxecute    
-
 document.getElementById("button5").addEventListener("click", afficheModale);
 
 
-//Le bouton deviendra vert au clic
+//Le bouton deviendra vert au clic (this. signifie "quand on clique sur CE bouton, selon le bouton")
 function changeColorAccept(){
     this.style.backgroundColor = "green";  
-    let buttonReverse = this.getAttribute("data-inverse");
-    document.getElementById(buttonReverse).style.backgroundColor = "#D3D3D3";
-    
+// Variable qui s'appelle buttonIdReverse et qui est égale au bouton "inverse" du bouton sur lequel on clique    
+    let buttonIdReverse = this.getAttribute("data-inverse");
+//Ici on vient mettre en gris le bouton opposé au bouton sur lequel on clique (et qui lui deviendra vert dans cette fonction)    
+    document.getElementById(buttonIdReverse).style.backgroundColor = "#D3D3D3";  
+
+// SAUVEGARDE DANS LE SYSTEME AVEC LE SETITEM DES BOUTONS ACCEPTER QUAND L'UTILISATEUR FAIT SON CHOIX    
+    localStorage.setItem(this.id,"green") 
+    localStorage.setItem(buttonIdReverse,"#D3D3D3")
+
 }
 
 //Quand on clique sur le bouton la fonction ci-dessus s'éxecute    
@@ -139,7 +130,6 @@ document.getElementById("button6").addEventListener("click", changeColorAccept);
 
 //Quand on clique sur le bouton la fonction ci-dessus s'éxecute    
 document.getElementById("button8").addEventListener("click", changeColorAccept);
-
 
 //Quand on clique sur le bouton la fonction ci-dessus s'éxecute    
 document.getElementById("button10").addEventListener("click", changeColorAccept);
@@ -152,32 +142,131 @@ document.getElementById("button10").addEventListener("click", changeColorAccept)
 //Le bouton deviendra rouge au clic
 function changeColorDeclined(){
     this.style.backgroundColor = "red";  
-    let buttonReverse = this.getAttribute("data-inverse");
-    document.getElementById(buttonReverse).style.backgroundColor = "#D3D3D3";
+// Variable qui s'appelle buttonIdReverse et qui est égale au bouton "inverse" du bouton sur lequel on clique    
+    let buttonIdReverse = this.getAttribute("data-inverse");
+//Ici on vient mettre en gris le bouton opposé au bouton sur lequel on clique (et qui lui deviendra rouge dans cette fonction)    
+    document.getElementById(buttonIdReverse).style.backgroundColor = "#D3D3D3";
+
+// SAUVEGARDE DANS LE SYSTEME AVEC LE SETITEM DES BOUTONS REFUSER QUAND L'UTILISATEUR FAIT SON CHOIX    
+    localStorage.setItem(this.id,"red");
+    localStorage.setItem(buttonIdReverse,"#D3D3D3");
+    
 }
 
 //Quand on clique sur le bouton la fonction ci-dessus s'éxecute    
 document.getElementById("button7").addEventListener("click", changeColorDeclined);
 
-
-
-
-
 //Quand on clique sur le bouton la fonction ci-dessus s'éxecute    
 document.getElementById("button9").addEventListener("click", changeColorDeclined);
-
-
 
 //Quand on clique sur le bouton la fonction ci-dessus s'éxecute    
 document.getElementById("button11").addEventListener("click", changeColorDeclined);
 
 
 
+// Ici la fenêtre modale disparaîtra
 function cacheFenetre(){
     fenetreModale.style.display = "none";
-    
 }
 
 //Quand on clique sur le quatrième bouton la fonction ci-dessus s'éxecute    
-
 document.getElementById("button12").addEventListener("click", cacheFenetre);
+
+
+
+//RÉCUPÉRATION DE LA SAUVEGARDE FAITE AVEC LE SETITEM AU DESSUS // ON VIENT CHERCHER LA SAUVEGARDE AVEC LE GETITEM AVEC UNE FONCTION POUR CHACUN DES BOUTONS
+
+
+//Fonction pour qu'au chargement de la page, le choix que l'utilisateur a fait précédemment s'affiche
+//Premier bouton ACCEPTER
+function userAccept1(){
+    let contenuPersoValidate = localStorage.getItem('button6');
+    if(contenuPersoValidate != null){
+        document.getElementById("button6").style.backgroundColor = contenuPersoValidate;
+    }
+    else{
+        document.getElementById("button6").style.backgroundColor = "#D3D3D3";
+    }
+}
+
+    userAccept1()
+
+
+
+//Fonction pour qu'au chargement de la page, le choix que l'utilisateur a fait précédemment s'affiche
+//Deuxième bouton ACCEPTER
+function userAccept2(){
+    let pubValidate = localStorage.getItem("button8");
+    if(pubValidate != null){
+        document.getElementById("button8").style.backgroundColor = pubValidate;
+    }
+    else{
+        document.getElementById("button8").style.backgroundColor = "D3D3D3";
+    }
+}
+    userAccept2()
+
+
+
+//Fonction pour qu'au chargement de la page, le choix que l'utilisateur a fait précédemment s'affiche
+//Troisième bouton ACCEPTER
+function userAccept3(){
+    let audianceValidate = localStorage.getItem("button10");
+    if(audianceValidate != null){
+        document.getElementById("button10").style.backgroundColor = audianceValidate;
+    }
+    else{
+        document.getElementById("button10").style.backgroundColor = "D3D3D3";
+    }
+}
+    userAccept3()
+
+
+
+
+//Fonction pour qu'au chargement de la page, le choix que l'utilisateur a fait précédemment s'affiche
+//Premier bouton REFUSER
+function userDeclined1(){
+    let contenuDeclined = localStorage.getItem("button7");
+    if(contenuDeclined != null){
+        document.getElementById("button7").style.backgroundColor = contenuDeclined;
+    }
+    else{
+        document.getElementById("button7").style.backgroundColor = "D3D3D3";
+    }
+}
+    userDeclined1()
+
+
+
+
+
+//Fonction pour qu'au chargement de la page, le choix que l'utilisateur a fait précédemment s'affiche
+//Deuxième bouton REFUSER
+function userDeclined2(){
+    let pubDeclined = localStorage.getItem("button9");
+    if(pubDeclined != null){
+        document.getElementById("button9").style.backgroundColor = pubDeclined;
+    }
+    else{
+        document.getElementById("button9").style.backgroundColor = "D3D3D3";
+    }
+}
+    userDeclined2()
+
+
+
+    
+
+//Fonction pour qu'au chargement de la page, le choix que l'utilisateur a fait précédemment s'affiche
+//Troisième bouton REFUSER
+function userDeclined3(){
+    let audianceDeclined = localStorage.getItem("button11");
+    if(audianceDeclined != null){
+        document.getElementById("button11").style.backgroundColor = audianceDeclined;
+    }
+    else{
+        document.getElementById("button11").style.backgroundColor = "D3D3D3";
+    }
+}
+    userDeclined3()
